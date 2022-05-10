ee14a702373b74cb7d0e804e394b5c32a44a00e1
cyberphoria <61635007+cyberphoria@users.noreply.github.com>
2022-05-10 17:59:25 -0400

    CommandTest: add ParameterizedTest

--- src/test/java/view/CommandTest.java
+++ src/test/java/view/CommandTest.java
@@ -1,10 +1,15 @@
 package view;
 
+import org.junit.jupiter.params.provider.EnumSource;
+import view.Command;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
+
 
 /**
  * CommandTest ensures that Command's search method functions correctly.
@@ -13,14 +18,13 @@ import static org.junit.jupiter.api.Assertions.assertNull;
  */
 public class CommandTest {
 
-    @Test
-    void testGetCommandValidInputs() {
-        for (Command command : Command.values()) {
+    @ParameterizedTest
+    @EnumSource(Command.class)
+    void testGetCommandValidInputs(Command command) {
             String commandName = command.getName();
             Command foundCommand = Command.getCommand(commandName);
             Assertions.assertNotNull(foundCommand);
             assertEquals(commandName, foundCommand.getName());
-        }
     }
 
     @Test
