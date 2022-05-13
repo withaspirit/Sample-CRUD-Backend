bfc33c28c1f4c29541448adcce34a82a8398e10a
Liam Tripp <61635007+cyberphoria@users.noreply.github.com>
2022-05-12 22:36:11 -0400

    Add InputMatcher (#11)
    Add InputMatcher class to handle matching the Command regexes to the user's input

---
+++ src/main/java/view/InputMatcher.java
@@ -0,0 +1,62 @@
+package view;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * InputMatcher is a helper class for DatabaseCLI that provides methods to read
+ * in user inputs.
+ *
+ * @author Liam Tripp
+ */
+public class InputMatcher {
+
+    /**
+     * Matches an input to one of the Command's Regexes.
+     *
+     * @param userInput the user's input
+     * @return a matcher matching the user's input, null otherwise
+     */
+    public Matcher matchInputToCommandRegex(String userInput) {
+        Matcher matcher = null;
+        for (Command command : Command.values()) {
+            matcher = getMatcher(command.getRegex(), userInput);
+            if (matcher.matches()) {
+                matcher.reset();
+                return matcher;
+            }
+        }
+        return matcher;
+    }
+
+    /**
+     * Returns a Matcher matching input to a given Regular Expression.
+     *
+     * @param regex a Regular Expression to capture
+     * @param userInput the input to the matcher
+     * @return the matcher after matching the userInput to the regex
+     */
+    public Matcher getMatcher(String regex, String userInput) {
+        Pattern createPattern = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
+        return createPattern.matcher(userInput);
+    }
+
+    /**
+     * Returns a String indicating whether the Matcher has an error.
+     * Also prepares the matcher to be used.
+     *
+     * @param matcher the matcher being examined
+     * @return a String with an error statement if there is an error, "" otherwise
+     */
+    public String validateMatcher(Matcher matcher) {
+        if (!matcher.matches()) {
+            return "Bad input formatting. Enter '" + Command.HELP.getName() +
+                    "' for options.";
+        }
+        matcher.reset();
+        if (!matcher.find()) {
+            return "Matcher.find() fail.";
+        }
+        return "";
+    }
+}
